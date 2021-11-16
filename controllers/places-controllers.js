6e751a120f66b1d1f999b88d5d755fc68eeafeb4
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-16 10:52:09 +0530

    update schemas

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -112,7 +112,7 @@ const createPlace = async (req, res, next) => {
     await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
-      'Creating place failed, please try again.',
+      'Creating place failed, please try again. ' + err,
       500
     );
     return next(error);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
+// const uniqueValidator = require('mongoose-unique-validator');
+//unique validator is not working. This is an old package that hasn't been updated
 const Schema = mongoose.Schema;
 
 
@@ -12,7 +12,7 @@ const userSchema = new Schema({
     places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place'}]
 });
 
-userSchema.plugin(uniqueValidator);
+// userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
 
